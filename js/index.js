ed8acf54bdcda0ea3b8e826a28dc565e7ea55214
Carlos Manuel <20203tn133@utez.edu.mx>
2021-07-05 11:18:06 -0500

    Intento de Examen

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var notas = {  
+    descripcion:"",
+};
+var filas =1;
+var columnas =1;
+
+var fecha = new Date();
+var horas = fecha.getHours();
+
+function crear(){
+    console.log ("se llamo a crear nueva tabla");
+    notas.descripcion = document.getElementById("descripcion").value;
+    actualizarFicha();
+}
+
+function actualizarFicha(){
+    document.getElementById("descrip").innerHTML = notas.descripcion;   
+}
+
+function crearTabla() {
+    var nt = document.getElementById("nota").value;
+    console.log(nt);
+
+    var body = document.getElementsByTagName("body")[0];
+
+    var tabla   = document.createElement("table");
+    var tabla   = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+
+    var hilera = document.createElement("tr");
+
+                
+    var celda = document.createElement("td");
+    var textoCelda = document.createTextNode(nt);
+    celda.appendChild(textoCelda);
+    hilera.appendChild(celda);
+                
+
+    tblBody.appendChild(hilera);
+    tabla.appendChild(tblBody);
+    body.appendChild(tabla);
+    tabla.setAttribute("border", "0");
+    tabla.setAttribute("style", "margin: 0 auto;");
+}
